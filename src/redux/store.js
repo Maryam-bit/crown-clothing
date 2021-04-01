b035609296637e4ca2f1b0ce4e23e6dd7bb52400
Maryam Noor <rajputmaryam5555@gmail.com>
2021-04-01 22:19:48 +0500

    donee

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,9 @@ import rootReducer from "./root-reducer"
 import {persistStore} from "redux-persist"
 const Middlewares = [];
 
-if(process.env.NODE_ENV =='development'){
-    Middlewares.push(logger)
-}
+// if(process.env.NODE_ENV =='development'){
+//     Middlewares.push(logger)
+// }
 
 const store = createStore(rootReducer, applyMiddleware(...Middlewares));
 const persistor = persistStore(store)
