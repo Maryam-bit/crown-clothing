4004924469ec83a5cc45e7428afd16584d28a950
Maryam Noor <rajputmaryam5555@gmail.com>
2021-04-01 22:26:35 +0500

    ready to commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from "redux-logger";
 import rootReducer from "./root-reducer"
 import {persistStore} from "redux-persist"
-const Middlewares = [];
+const Middlewares = [logger];
 
 // if(process.env.NODE_ENV =='development'){
 //     Middlewares.push(logger)
